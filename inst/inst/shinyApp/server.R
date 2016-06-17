614b55aa0e0698197016b44e1ad1b08d77b6c980
Rick Farouni <rfarouni@gmail.com>
2016-06-17 14:52:40 -0400

    added shiny

---
+++ inst/inst/shinyApp/server.R
@@ -0,0 +1,89 @@
+# utility functions
+
+
+
+################################################################
+
+shinyServer(function(input, output, session) {
+
+  ## tabName"definerep"
+
+  #  tabPanel "Load CSV"
+
+  inputFilePath <- reactive({
+    if (!is.null(input$file)) {
+      # User has not uploaded a file
+      return(input$file)
+    }
+  })
+
+  csvFile <- reactive({
+    if (!is.null(input$file)) {
+      inputPathObj <- inputFilePath()
+      outputCSV <- loadCSVFile(inputPathObj$datapath)
+      return(outputCSV)
+    }
+  })
+
+  output$table1 <- renderDataTable({
+    if (!is.null(input$file)){
+      csvFile()[,-1]
+    }
+  }, options = list(searching = FALSE,
+                    paging = FALSE))
+
+
+
+
+  #####################################################
+
+  #  tabPanel "merge"
+
+  peaks <- reactive({
+    if (!is.null(input$file)) {
+      fileIn <- csvFile()
+      outputBed <- loadBedFiles(fileIn)
+      return(outputBed)
+    }
+  })
+
+  mergedPeaks <- eventReactive(input$buttonmerge, {
+    object <-  peaks()
+    object2 <- getReproducePeaks(object, input$numOverlap)
+    return(object2)
+  })
+
+  observeEvent(input$buttonmerge, {
+    withProgress(message = 'Merging in progress',
+                 detail = 'This may take a while...',
+                 value = 0,
+                 {
+                   setProgress(value = 0, detail = "loading new dataset")
+                   for (i in 1:2) {
+                     incProgress(1 / 4)
+                     Sys.sleep(0.05)
+                   }
+                   mergedPeaks()
+                   for (i in 1:2) {
+                     incProgress(1 / 4)
+                     Sys.sleep(0.1)
+                   }
+                   setProgress(value = 1, detail = "Done!")
+                 })
+  })
+
+
+  output$table2 <- renderDataTable({
+    mergedPeaks()$sizePeaks
+  }, options = list(searching = FALSE,
+                    paging = FALSE))
+
+
+
+
+  ####################################
+  #  tabPanel "plot"
+
+  
+
+})
