4f64679453cb47b76eecefbc33f6022bbd830945
Rick Farouni <rfarouni@gmail.com>
2016-06-17 22:43:43 -0400

    fixed no visible binding for global variable RCHECK error

--- R/shinyplots.R
+++ R/shinyplots.R
@@ -1,23 +1,17 @@
 
 #' plot barplot
+#' @param sizePeaks consPeaksStats
 #' @export
 plotCountSummary <- function(sizePeaks) {
-  dat <- tidyr::gather(sizePeaks,sample,count,-1)
-
+  dat <- tidyr::gather(sizePeaks,'sample','count',-1)
   p   <-
-    ggplot(data = dat, aes(x = PeaksFile, y = count, fill = sample)) +
+    ggplot(data = dat,
+           aes_string(x = 'PeaksFile', y = 'count', fill='sample')) +
     geom_bar(colour = "black",
              stat = "identity",
              position = position_dodge())
   return(p)
 }
-
-
-
-
-
-
-
 
 
 
