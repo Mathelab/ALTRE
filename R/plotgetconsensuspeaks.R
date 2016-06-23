3bafd3d50bce7b40405a7a0ce4b495de4667eac6
Rick Farouni <rfarouni@gmail.com>
2016-06-23 11:40:37 -0400

    fixed note message

--- R/plotgetconsensuspeaks.R
+++ R/plotgetconsensuspeaks.R
@@ -14,24 +14,37 @@
 #' @export
 
 plotConsensusPeaks <- function(samplepeaks) {
-  dfstats=samplepeaks$consPeaksStats
-#  dfstats$Replicate=dfstats$PeakType
-#  dfstats=dfstats[,-which(colnames(dfstats)=="PeakType")]
-#  mydf=na.omit(reshape2::melt(dfstats))
-  mydf=tidyr::gather(dfstats,CellType,count,2:3)
+  dfstats = samplepeaks$consPeaksStats
+  #  dfstats$Replicate=dfstats$PeakType
+  #  dfstats=dfstats[,-which(colnames(dfstats)=="PeakType")]
+  #  mydf=na.omit(reshape2::melt(dfstats))
+  mydf = tidyr::gather(dfstats, 'CellType', 'count', 2:3)
 
-  p <- ggplot(data=mydf,aes(x=mydf$CellType, y=mydf$count,fill=mydf$PeakType)) +
-	geom_bar(stat="identity",position=position_dodge()) +
-	geom_text(aes(label = mydf$count,x=mydf$CellType, y=mydf$count,ymax=mydf$count),
-		position=position_dodge(width=1),size=3,hjust=0.5,vjust=-1.5) +
-	scale_colour_manual(values = c("red", "dark grey")) +
-	labs(fill="") +
-	theme_bw(base_size = 15) +
-	theme(panel.grid.major = element_blank(),
+  p <-
+    ggplot(data = mydf,
+           aes_string(
+             x = 'CellType',
+             y = 'count',
+             fill = 'PeakType') ) +
+    geom_bar(stat = "identity", position = position_dodge()) +
+    geom_text(
+      aes_string(
+        label = 'count',
+        x = 'CellType',
+        y = 'count',
+        ymax = 'count'
+      ),
+      position = position_dodge(width = 1),
+      size = 3,
+      hjust = 0.5,
+      vjust = -1.5
+    ) +
+    scale_colour_manual(values = c("red", "dark grey")) +
+    labs(fill = "") +
+    theme_bw(base_size = 15) +
+    theme(panel.grid.major = element_blank(),
           panel.grid.minor = element_blank()) +
-	labs(x = "Sample Type", y = "Number of Peaks (Regulatory Regions)")
+    labs(x = "Sample Type", y = "Number of Peaks (Regulatory Regions)")
 
   return(p)
 }
-
-
