9c9fefe36e9e73d2009d8c9bc6c9c3249c5fef63
Rick Farouni <rfarouni@gmail.com>
2016-07-03 13:52:39 -0400

    added download button

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -356,7 +356,7 @@ body <- dashboardBody(
 
 
                 actionButton("buttoncat", strong("Categorize Altered Regions")),
-                conditionalPanel("input.catAlteredStatus",
+                conditionalPanel("input.buttoncat > 0 ",
                                  hr(),
                                   downloadButton("downloadData",
                                                strong("Download BED File")
