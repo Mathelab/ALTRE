2a23bbf70222e76775d3ace77143cf5c3f2e5022
rick <rfarouni@gmail.com>
2016-08-31 22:58:38 -0400

    reindented code

--- R/8a_runGREAT.R
+++ R/8a_runGREAT.R
@@ -27,15 +27,14 @@
 
 runGREAT <- function(peaks,
                      #	peaktype="All",
-                     species="hg19",
-                     rule="basalPlusExt",
-                     adv_upstream=5.0,
-                     adv_downstream=1.0,
-                     adv_span=1000.0,
-                     adv_twoDistance=1000.0,
-                     adv_oneDistance=1000.0,
-                     pathway_category="GO") {
-
+                     species = "hg19",
+                     rule = "basalPlusExt",
+                     adv_upstream = 5.0,
+                     adv_downstream = 1.0,
+                     adv_span = 1000.0,
+                     adv_twoDistance = 1000.0,
+                     adv_oneDistance = 1000.0,
+                     pathway_category = "GO") {
   # Check that peaktype entry is allowable and grab peaks for analysis
   #  if (is.na(match(peaktype,c("All","Experiment Specific", "Shared", "Ambiguous",
   #	"Reference Specific")))) {
@@ -47,29 +46,48 @@ runGREAT <- function(peaks,
   #  else {
   #	mypeaks = as.data.frame(peaks$analysisresults)[which(peaks$analysisresults==peaktype),
   #		c("chr","start","stop")]}
-  if (is.na(match(rule, c("basalPlusExt", "twoClosest", "oneClosest")))) {
+  if (is.na(match(rule, c(
+    "basalPlusExt", "twoClosest", "oneClosest"
+  )))) {
     stop("rule must be either 'basalPlusExt', 'twoClosest', 'oneClosest'")
   }
 
-  mygreat=list()
+  mygreat = list()
   for (i in c("Experiment Specific", "Reference Specific", "Shared")) {
-    print(paste("Running",i))
-    mypeaks=as.data.frame(peaks$analysisresults)[which(peaks$analysisresults$REaltrecateg==i),
-                                                 c("chr","start","stop")]
-    ilabel=gsub(" ","_",i)
+    print(paste("Running", i))
+    mypeaks = as.data.frame(peaks$analysisresults)[which(peaks$analysisresults$REaltrecateg ==
+                                                           i),
+                                                   c("chr", "start", "stop")]
+    ilabel = gsub(" ", "_", i)
     # Run GREAT
     if (rule == "basalPlusExt") {
-      mygreat[[ilabel]]=rGREAT::submitGreatJob(mypeaks, species=species,adv_span=adv_span,
-                                               rule="basalPlusExt", adv_upstream=adv_upstream,adv_downstream=adv_downstream,
-                                               request_interval=20)
+      mygreat[[ilabel]] = rGREAT::submitGreatJob(
+        mypeaks,
+        species = species,
+        adv_span = adv_span,
+        rule = "basalPlusExt",
+        adv_upstream = adv_upstream,
+        adv_downstream = adv_downstream,
+        request_interval = 20
+      )
     }
-    if (rule=="twoClosest") {
-      mygreat[[ilabel]]=rGREAT::submitGreatJob(mypeaks, species=species,
-                                               rule="twoClosest", adv_twoDistance=adv_twoDistance,request_interval=20)
+    if (rule == "twoClosest") {
+      mygreat[[ilabel]] = rGREAT::submitGreatJob(
+        mypeaks,
+        species = species,
+        rule = "twoClosest",
+        adv_twoDistance = adv_twoDistance,
+        request_interval = 20
+      )
     }
-    if (rule=="oneClosest") {
-      mygreat[[ilabel]]=rGREAT::submitGreatJob(mypeaks, species=species,
-                                               rule="oneClosest", adv_twoDistance=adv_oneDistance,request_interval=20)
+    if (rule == "oneClosest") {
+      mygreat[[ilabel]] = rGREAT::submitGreatJob(
+        mypeaks,
+        species = species,
+        rule = "oneClosest",
+        adv_twoDistance = adv_oneDistance,
+        request_interval = 20
+      )
     }
   } # end looping through peak types
   return(mygreat)
@@ -97,53 +115,65 @@ runGREAT <- function(peaks,
 #' represents one pathway type (e.g. "GO Molecular Function")
 
 processPathways <- function(GREATpath,
-                            pathway_category="GO",
-                            adjustby="bonferroni",
-                            test="Binom",
-                            enrichcutoff=2,
-                            adjpvalcutoff=0.05) {
-
-  finaloutput=list()
+                            pathway_category = "GO",
+                            adjustby = "bonferroni",
+                            test = "Binom",
+                            enrichcutoff = 2,
+                            adjpvalcutoff = 0.05) {
+  finaloutput = list()
   for (job in names(GREATpath)) {
-    if(!is(GREATpath[[job]], "GreatJob")) {
-      stop("GREATpath is not a list of 'GreatJob' objects.
-           Input should be the output of runGREAT()")
+    if (!is(GREATpath[[job]], "GreatJob")) {
+      stop(
+        "GREATpath is not a list of 'GreatJob' objects.
+        Input should be the output of runGREAT()"
+      )
     }
 
-    output= rGREAT::getEnrichmentTables(GREATpath[[job]],category=pathway_category)
-    names(output)=gsub(" ","_",names(output))
-    stats=data.frame(Pathway=names(output),NumSig=rep(NA,length(names(output))))
+    output = rGREAT::getEnrichmentTables(GREATpath[[job]], category = pathway_category)
+    names(output) = gsub(" ", "_", names(output))
+    stats = data.frame(Pathway = names(output),
+                       NumSig = rep(NA, length(names(output))))
 
     for (i in names(output)) {
-      output[[i]]$Binom_adj_PValue=stats::p.adjust(output[[i]]$"Binom_Raw_PValue",
-                                                   method=adjustby)
-      output[[i]]$Hyper_adj_PValue=stats::p.adjust(output[[i]]$"Hyper_Raw_PValue",
-                                                   method=adjustby)
-      if(test=="Both") {
-        keepers=base::Reduce(intersect, list(which(output[[i]]$Binom_Fold_Enrichment>enrichcutoff),
-                                             which(output[[i]]$Hyper_Fold_Enrichment>enrichcutoff),
-                                             which(output[[i]]$Binom_adj_PValue<=adjpvalcutoff),
-                                             which(output[[i]]$Hyper_adj_PValue<=adjpvalcutoff)))
+      output[[i]]$Binom_adj_PValue = stats::p.adjust(output[[i]]$"Binom_Raw_PValue",
+                                                     method = adjustby)
+      output[[i]]$Hyper_adj_PValue = stats::p.adjust(output[[i]]$"Hyper_Raw_PValue",
+                                                     method = adjustby)
+      if (test == "Both") {
+        keepers = base::Reduce(intersect, list(
+          which(output[[i]]$Binom_Fold_Enrichment > enrichcutoff),
+          which(output[[i]]$Hyper_Fold_Enrichment >
+                  enrichcutoff),
+          which(output[[i]]$Binom_adj_PValue <=
+                  adjpvalcutoff),
+          which(output[[i]]$Hyper_adj_PValue <=
+                  adjpvalcutoff)
+        ))
       }
-      else if (test=="Binom") {
-        keepers=base::Reduce(intersect, list(which(output[[i]]$Binom_Fold_Enrichment>enrichcutoff),
-                                             which(output[[i]]$Binom_adj_PValue<=adjpvalcutoff)))
+      else if (test == "Binom") {
+        keepers = base::Reduce(intersect, list(
+          which(output[[i]]$Binom_Fold_Enrichment > enrichcutoff),
+          which(output[[i]]$Binom_adj_PValue <=
+                  adjpvalcutoff)
+        ))
       }
-      else if (test=="Hyper") {
-        keepers=base::Reduce(intersect, list(which(output[[i]]$Hyper_Fold_Enrichment>enrichcutoff),
-                                             which(output[[i]]$Hyper_adj_PValue<=adjpvalcutoff)))
+      else if (test == "Hyper") {
+        keepers = base::Reduce(intersect, list(
+          which(output[[i]]$Hyper_Fold_Enrichment > enrichcutoff),
+          which(output[[i]]$Hyper_adj_PValue <=
+                  adjpvalcutoff)
+        ))
       }
       else {
         stop("test should be 'Both', 'Binom', or 'Hyper'")
       }
       print(length(keepers))
-      output[[i]]=output[[i]][keepers,]
-      output[[i]]=output[[i]][base::order(output[[i]]$Binom_adj_PValue),]
-      stats$NumSig[which(stats$Pathway==i)]=
-        length(which(output[[i]]$Binom_adj_PValue<=adjpvalcutoff))
+      output[[i]] = output[[i]][keepers, ]
+      output[[i]] = output[[i]][base::order(output[[i]]$Binom_adj_PValue), ]
+      stats$NumSig[which(stats$Pathway == i)] =
+        length(which(output[[i]]$Binom_adj_PValue <= adjpvalcutoff))
     }
-    finaloutput[[job]]=list(Sig_Pathways=output, stats=stats)
+    finaloutput[[job]] = list(Sig_Pathways = output, stats = stats)
     } # end looping through each GREAT job
   return(finaloutput)
 } # end function
-
