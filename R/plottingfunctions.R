af29e9160ce3d8230fb1bb3648a5e691d1278b56
mathelab <ewy.mathe@osumc.edu>
2016-09-15 13:45:19 -0400

    Fixed typo

--- R/plottingfunctions.R
+++ R/plottingfunctions.R
@@ -1531,7 +1531,7 @@ plotGREATenrich <- function(input,
       maxWidth = 200,
       x = -10, # 90
       y = 100, # 70
-      padding=2,
+      padding=2
       #title = list(text="p-value")
     ) %>%
     hc_exporting(enabled = TRUE)
